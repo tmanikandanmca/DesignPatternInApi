828b286c88129bbdd27809d2fccd9c3f4368491a
Manikandan Thiyagarajan <67586339+tmanikandanmca@users.noreply.github.com>
2024-01-15 19:56:54 +0530

    Project added

--- AdventureWorks.Domain/obj/Debug/net8.0/AdventureWorks.Domain.AssemblyInfo.cs
+++ AdventureWorks.Domain/obj/Debug/net8.0/AdventureWorks.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventureWorks.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6653d68061bcca16c1f248656ce0cfcfc1bc981")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventureWorks.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventureWorks.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Chapter-1/Controllers/CurrencyController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chapter_1.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CurrencyController : ControllerBase
+{ 
+	public CurrencyController()
+	{
+			
+	}
+}
