d376ddd5d8c7f230e8959b47354db3937cf40430
Manikandan Thiyagarajan <67586339+tmanikandanmca@users.noreply.github.com>
2024-01-15 21:36:41 +0530

    decorate pattern added with cache

--- Chapter-1/Controllers/CurrencyController.cs
+++ Chapter-1/Controllers/CurrencyController.cs
@@ -1,14 +1,46 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AdventureWorks.Application.ServicesInterfaces;
+using Chapter_1.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Chapter_1.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
 public class CurrencyController : ControllerBase
-{ 
-	public CurrencyController()
-	{
-			
-	}
+{
+    private readonly ICurrencyService _service;
+    private readonly ILoggerFactory _loggerFactory;
+    private readonly ILogger<CurrencyController> _logger;
+    private IMemoryCache _cache;
+
+    public CurrencyController(ICurrencyService service
+        , IMemoryCache cache
+        , ILoggerFactory loggerFactory
+        , ILogger<CurrencyController> logger)
+    {
+        _loggerFactory = loggerFactory;
+        _cache= cache;
+        _logger = _loggerFactory.CreateLogger<CurrencyController>();
+        ICurrencyService _cwithLogger = new CurrencyWithLogger(_loggerFactory.CreateLogger<CurrencyWithLogger>(), service);
+        ICurrencyService _cwithCache = new CurrencyWithCache(_cwithLogger, cache);
+
+        _service = _cwithCache;
+    }
+
+
+    [HttpGet("GetAll")]
+    public IActionResult GetAllResult()
+    {
+        return Ok(_service.GetAllCurrencies());
+    }
+
+    [HttpGet("GetById{id}")]
+    public IActionResult GetById(int id)
+    {
+        return Ok(_service.GetCurrencyById(id));
+    }
+
+
+
 }
---
+++ Chapter-1/Services/CurrencyWithCache.cs
@@ -0,0 +1,68 @@
+﻿using AdventureWorks.Application.ServicesInterfaces;
+using AdventureWorks.Domain.Models;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Chapter_1.Services;
+
+public class CurrencyWithCache : ICurrencyService
+{
+    private readonly ICurrencyService _currencyService;
+    private IMemoryCache _cache;
+    public CurrencyWithCache(ICurrencyService currencyService, IMemoryCache cache)
+    {
+        _currencyService = currencyService;
+        _cache = cache;
+    }
+
+
+    public bool AddCurrency(Currency currency)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool DeleteCurrency(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Currency> GetAllCurrencies()
+    {
+        string cacheKey = $"GetCurrencyById::All";
+        if (_cache.TryGetValue<List<Currency>>(cacheKey, out var values))
+        {
+            return values;
+        }
+        else
+        {
+            var res = _currencyService.GetAllCurrencies();
+            _cache.Set<List<Currency>>(cacheKey, res, TimeSpan.FromMinutes(30));
+            return res;
+        }
+    }
+
+    public Currency GetCurrencyById(int id)
+    {
+        string cacheKey = $"GetCurrencyById::{id}";
+        if (_cache.TryGetValue<Currency>(cacheKey, out var values))
+        {
+            return values;
+        }
+        else
+        {
+            var res = _currencyService.GetCurrencyById(id);
+            _cache.Set<Currency>(cacheKey, res, TimeSpan.FromMinutes(30));
+            return res;
+        }
+
+    }
+
+    public Currency GetCurrencyByName(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool UpdateCurrency(Currency currency)
+    {
+        throw new NotImplementedException();
+    }
+}
