8f033488a6f6489d50ec2ddd855b9159379b8d19
Manikandan Thiyagarajan <67586339+tmanikandanmca@users.noreply.github.com>
2024-01-21 19:43:49 +0530

    Products added

---
+++ AdventureWorks.Application/Services/ProductService.cs
@@ -0,0 +1,33 @@
+﻿using AdventureWorks.Application.ServicesInterfaces;
+using AdventureWorks.Domain.Models;
+using AdventureWorks.Infrastructure.Repositories;
+
+namespace AdventureWorks.Application.Services;
+
+public class ProductService : IProductService
+{
+    private readonly IProductsRepository repo;
+    public ProductService(IProductsRepository repo)
+    {
+        this.repo = repo;
+    }
+    public Task<IEnumerable<ProductDTO>> GetProducts()
+    {
+       var res = repo.GetAll();
+        //var response = repo.GetAll().Select(x => new ProductDTO { }).ToList();
+        //    .Select(e=> new  ProductDTO {  
+        //    ProductId = e.ProductId, 
+        //    ProductName = e.ProductName,
+        //    description=e.ProductNumber,
+        //    unitPrice=e.ListPrice,
+        //    categoryId=e.ProductSubcategoryID,
+        //}).ToList();
+        List<ProductDTO> products = [];
+        throw new NotImplementedException();
+    }
+
+    public Task<ProductDTO> GetProductsById(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AdventureWorks.Application/ServicesInterfaces/IProductService.cs
@@ -0,0 +1,19 @@
+﻿namespace AdventureWorks.Application.ServicesInterfaces;
+
+public interface IProductService
+{
+    Task<IEnumerable<ProductDTO>> GetProducts();
+
+    Task<ProductDTO> GetProductsById(int id);
+}
+
+public record ProductDTO
+{
+    public int productId { get; set; }
+    public string name { get; set; }
+    public string description { get; set; }
+    public int unitPrice {  get; set; }
+    public string categoryId {  get; set; }
+    public string category { get; set; }
+}
+
